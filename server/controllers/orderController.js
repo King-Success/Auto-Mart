2142ec7536336679e5556465cc66dbc3d71d7b9d
The Stoic Developer <code4kings@gmail.com>
2019-05-30 09:19:59 +0100

    Add user (order owner) can update order amount

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -28,6 +28,32 @@ class OrderController {
       return res.status(500).json({ error: true, message: 'Internal server error' });
     }
   }
+
+  static async updateOrderPrice(req, res) {
+    try {
+      const { orderId } = req.params;
+      const { amount } = req.body;
+      let order = orderModel.find(order => order.id === orderId);
+      if (!order) {
+        return res.status(400).json({ status: 400, message: `Purchase order with id: ${orderId} does not exist` });
+      }
+      order = { ...order, amount };
+      for (let i = 0; i < orderModel.length; i += 1) {
+        if (orderModel[i].id === orderId) {
+          orderModel.splice(i, 1);
+          orderModel.push(order);
+          return res.status(200).json({
+            status: 200,
+            data: [order],
+            message: 'Order amount updated successfully',
+          });
+        }
+      }
+      return res.status(500).json({ status: 500, error: 'Oops, something happend, try again' });
+    } catch (err) {
+      return res.status(500).json({ status: 500, error: 'Internal Server error' });
+    }
+  }
 }
 
 export default OrderController;
--- server/middlewares/orderValidator.js
+++ server/middlewares/orderValidator.js
@@ -1,4 +1,5 @@
 import Helpers from '../helpers';
+import OrderModel from '../models/order'
 
 const { extractErrors } = Helpers;
 
@@ -15,5 +16,34 @@ class OrderValidator {
         }
         return next();
     }
+
+    static isOrderOwner(req, res, next) {
+        const { id: buyerId } = req.body.tokenPayload;
+        const { orderId } = req.params;
+        try {
+          const order = OrderModel.find(odr => odr.id === orderId);
+          if (order) {
+            if (order.buyer === buyerId) {
+              return next();
+            }
+            return res.status(400).json({ status: 400, errors: 'Permission denied, you can only update price of orders you made' });
+          }
+          return res.status(400).json({ status: 400, errors: `Order with id: ${orderId} does not exist` });
+        } catch (error) {
+            console.log(error)
+          return res.status(500).json({ status: 500, errors: 'Internal server error' });
+        }
+      }
+
+      static validateAmount(req, res, next) {
+        req.checkBody('amount', 'Order amount is required').notEmpty().trim().isCurrency({ allow_negatives: false, require_decimal: true })
+        .withMessage('Order amount must be a valid number in two decimal place, e.g 123000.00');
+    
+        const errors = req.validationErrors();
+        if (errors) {
+          return res.status(400).json({ status: 400, errors: extractErrors(errors) });
+        }
+        return next();
+      }
 }
 export default OrderValidator;
