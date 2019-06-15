25e9e69b39941fb7b94d66163d3b9a6bca17cc12
The Stoic Developer <code4kings@gmail.com>
2019-06-15 16:26:57 +0100

    feature(Orders): update 'user can make car purchase order route' to use database instead of data-structures [Finishes #166695691]

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,29 +1,20 @@
-import shortId from 'shortid';
 import orderModel from '../models/order';
 
 class OrderController {
-  /**
-     *
-     * @param {object} req - request
-     * @param {object} res - response
-     */
+
   static async createOrder(req, res) {
-    /* istanbul ignore next */
-    const id = shortId.generate();
-    const status = 'pending';
-    const createdOn = new Date();
     try {
       const { id: buyer } = req.body.tokenPayload;
       const { carId, amount } = req.body;
-      const order = {
-        id, buyer, carId, amount, status, createdOn,
-      };
-      orderModel.push(order);
-      return res.status(201).json({
-        status: 201,
-        data: order,
-        message: 'Order created successfully',
-      });
+      const values = [buyer, carId, amount];
+      const order = await orderModel.create(values);
+      if (order) {
+        return res.status(201).json({
+          status: 201,
+          data: [order],
+          message: 'Car order created successfully',
+        });
+      }
     } catch (err) {
       return res.status(500).json({ error: true, message: 'Internal server error' });
     }
--- server/models/order.js
+++ server/models/order.js
@@ -1,3 +1,23 @@
-const OrderModel = [];
+import pool from '../config/connection';
 
-export default OrderModel;
+class Order {
+  static async create(values) {
+    const client = await pool.connect();
+    let order;
+    const text = `INSERT INTO orders(buyer, carId, amount)
+      VALUES($1, $2, $3) RETURNING *`;
+    try {
+      order = await client.query({ text, values });
+      if (order.rowCount) {
+        order = order.rows[0];
+        return order;
+      }
+      return false;
+    } catch (err) {
+      throw err;
+    } finally {
+      await client.release();
+    }
+  }
+}
+export default Order;
