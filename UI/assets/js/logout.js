a41c17c1a4c16e9100a0317ff12f94fe71305180
The Stoic Developer <code4kings@gmail.com>
2019-06-26 21:22:44 +0100

    chore(Synthax): add a new line to the end of each file

--- UI/assets/js/logout.js
+++ UI/assets/js/logout.js
@@ -1,7 +1,6 @@
 const logoutButton = document.getElementById('logout');
 
 const logout = (e) => {
-    console.log('yaaa')
     e.preventDefault()
     localStorage.removeItem('user')
     localStorage.removeItem('token')
