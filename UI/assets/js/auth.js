5d6ff3fb4c9afe4a15d1ce356ac4791e23475102
The Stoic Developer <code4kings@gmail.com>
2019-07-01 00:10:24 +0100

    feature(Navigation/Auth): implement feature where nav links changes dynamically based on user role and file location [Finishes #167001412]

--- UI/assets/js/auth.js
+++ UI/assets/js/auth.js
@@ -1,30 +1,38 @@
-const userToken = localStorage.getItem('token');
+(function auth() {
+  const userToken = localStorage.getItem('token');
+  const isAdmin = localStorage.getItem('admin')
 
-const validateToken = (token) => {
-  if (!token) window.location = 'login.html';
-  if (token) {
-    const data = {
-       token 
-      };
-    const config = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    };
-    const url = 'https://andela-auto-mart.herokuapp.com/api/v1/auth/validateToken';
-    fetch(url, config)
-      .then(res => res.json())
-      .then((result) => {
-        if (result.status === 200) {
-          return true;
-        }
-        window.location = 'login.html';
-      })
-      .catch((err) => { 
-        console.log(err)
-        if (err) return false; 
-      });
+  if (isAdmin) {
+    const profile = document.getElementById('profile')
+    if (profile) profile.style.display = 'none'
   }
-};
 
-validateToken(userToken);
+  const validateToken = (token) => {
+    if (!token) window.location = 'login.html';
+    if (token) {
+      const data = {
+        token
+      };
+      const config = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      };
+      const url = 'https://andela-auto-mart.herokuapp.com/api/v1/auth/validateToken';
+      fetch(url, config)
+        .then(res => res.json())
+        .then((result) => {
+          if (result.status === 200) {
+            return true;
+          }
+          window.location = 'login.html';
+        })
+        .catch((err) => {
+          console.log(err)
+          if (err) return false;
+        });
+    }
+  };
+
+  validateToken(userToken);
+})()
--- UI/assets/js/profile.js
+++ UI/assets/js/profile.js
@@ -1,36 +1,40 @@
-const carsGrid = document.getElementById('cars-grid');
-const avatar = document.getElementById('avater');
-const user = document.getElementById('user-name');
-const priceField = document.getElementById('update-price')
-const updateButton = document.getElementById('submit-btn')
-const modalError = document.querySelector('.modal-error')
+(function profile() {
+    const carsGrid = document.getElementById('cars-grid');
+    const avatar = document.getElementById('avater');
+    const user = document.getElementById('user-name');
+    const priceField = document.getElementById('update-price');
+    const updateButton = document.getElementById('submit-btn');
+    const modalError = document.querySelector('.modal-error');
+    const spinner = document.getElementById('spinner');
 
-const nodes = [modalError]
-const fields = [priceField]
-const activeUser = JSON.parse(localStorage.getItem('user'));
-const token = localStorage.getItem('token');
+    spinner.style.display = 'inline-block';
 
-avatar.style.background = user.passporturl || 'url(assets/img/avatar.png)';
-avatar.style.backgroundSize = 'cover'
-user.textContent = `${activeUser.firstname} ${activeUser.lastname}`;
-const url = 'https://andela-auto-mart.herokuapp.com/api/v1/car/history';
-const config = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + token,
-    },
-};
-fetch(url, config)
-    .then(res => res.json())
-    .then((res) => {
-        carsGrid.innerHTML = ''
-        if (res.status === 200) {
-            const { data } = res;
-            if (data.length) {
-                let grid;
-                data.sort((a, b) => a.id - b.id).forEach((car) => {
-                    grid = `<div class="grid">
+    const nodes = [modalError]
+    const fields = [priceField]
+    const activeUser = JSON.parse(localStorage.getItem('user'));
+    const token = localStorage.getItem('token');
+
+    avatar.style.background = user.passporturl || 'url(assets/img/avatar.png)';
+    avatar.style.backgroundSize = 'cover'
+    user.textContent = `${activeUser.firstname} ${activeUser.lastname}`;
+    const url = 'https://andela-auto-mart.herokuapp.com/api/v1/car/history';
+    const config = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+        },
+    };
+    fetch(url, config)
+        .then(res => res.json())
+        .then((res) => {
+            carsGrid.innerHTML = ''
+            if (res.status === 200) {
+                const { data } = res;
+                if (data.length) {
+                    let grid;
+                    data.sort((a, b) => a.id - b.id).forEach((car) => {
+                        grid = `<div class="grid">
                     <div class="grid__image">
                         <img src="${car.mainimageurl}" alt="car image" data-image="${car.id}">
                     </div>
@@ -53,66 +57,67 @@ fetch(url, config)
                         </div>
                     </div>
                 </div>`;
-                    carsGrid.insertAdjacentHTML('beforeend', grid);
+                        carsGrid.insertAdjacentHTML('beforeend', grid);
 
-                });
-                installModal()
-                updateButton.addEventListener('click', updatePrice);
-                return false
+                    });
+                    installModal()
+                    updateButton.addEventListener('click', updatePrice);
+                    return false
+                } else {
+                    carsGrid.insertAdjacentHTML('beforeend', '<p style="text-align: center;" >You currently have no advertisement to display</p>');
+                    return false
+                }
             } else {
-                carsGrid.insertAdjacentHTML('beforeend', '<p>You currently have no advertisement to display</p>');
-                return false
+                carsGrid.insertAdjacentHTML('beforeend', '<p style="text-align: center;" >You haven\'t posted an advert yet')
             }
-        } else {
-            carsGrid.insertAdjacentHTML('beforeend', '<p>Oops, looks like you haven\'t posted an advert yet')
-        }
-    })
-    .catch((err) => {
-        console.log(err);
-        carsGrid.innerHTML = ''
-        carsGrid.insertAdjacentHTML('beforeend', '<p style="text-align: center;">Oops, something happend, couldn\'t load your adverts, check your internet and refresh page to reload')
-    });
-
-
-const updatePrice = (e) => {
-    wipeAlert(nodes, updateButton, 'update')
-    const carId = clickModalEvent.target.getAttribute('data-id');
-    updateButton.textContent = 'saving...'
-    if (priceField.value === '') {
-        wipeAlert(nodes);
-        modalError.style.display = 'block'
-        modalError.textContent = 'Price is required';
-        priceField.focus();
-        updateButton.textContent = 'update'
-        return false;
-    }
-    const url = `https://andela-auto-mart.herokuapp.com/api/v1/car/${carId}/price`;
-    const token = localStorage.getItem('token');
-    const config = {
-        method: 'PATCH',
-        body: JSON.stringify({ price: priceField.value }),
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + token,
-
-        }
-    };
-    fetch(url, config)
-        .then(res => res.json())
-        .then((result) => {
-            if (result.status !== 200) {
-                wipeAlert(nodes, updateButton, 'update')
-                modalError.style.display = 'block'
-                modalError.textContent = result.message ? result.message : result.error ? result.error : result.errors ? result.errors[0] : '';
-                return false
-            }
-            wipeAlert(nodes, updateButton, 'update')
-            wipeForm(fields);
-            success.style.display = 'block';
-            success.textContent = 'Price updated successfully';
-            setTimeout(() => window.location.reload(true), 1000)
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
+            carsGrid.innerHTML = ''
+            carsGrid.insertAdjacentHTML('beforeend', '<p style="text-align: center;">Oops, something happend, couldn\'t load your adverts, check your internet and refresh page to reload')
         });
-}
+
+
+    const updatePrice = () => {
+        wipeAlert(nodes, updateButton, 'update')
+        const carId = clickModalEvent.target.getAttribute('data-id');
+        updateButton.textContent = 'saving...'
+        if (priceField.value === '') {
+            wipeAlert(nodes);
+            modalError.style.display = 'block'
+            modalError.textContent = 'Price is required';
+            priceField.focus();
+            updateButton.textContent = 'update'
+            return false;
+        }
+        const url = `https://andela-auto-mart.herokuapp.com/api/v1/car/${carId}/price`;
+        const token = localStorage.getItem('token');
+        const config = {
+            method: 'PATCH',
+            body: JSON.stringify({ price: priceField.value }),
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+
+            }
+        };
+        fetch(url, config)
+            .then(res => res.json())
+            .then((result) => {
+                if (result.status !== 200) {
+                    wipeAlert(nodes, updateButton, 'update')
+                    modalError.style.display = 'block'
+                    modalError.textContent = result.message ? result.message : result.error ? result.error : result.errors ? result.errors[0] : '';
+                    return false
+                }
+                wipeAlert(nodes, updateButton, 'update')
+                wipeForm(fields);
+                success.style.display = 'block';
+                success.textContent = 'Price updated successfully';
+                setTimeout(() => window.location.reload(true), 1000)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    }
+})()
