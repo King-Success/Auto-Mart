ed4ece02b7bda505af543d9a20107936bc14b08d
The Stoic Developer <code4kings@gmail.com>
2019-07-15 14:19:50 +0100

    bug(Auto grader): log errors to get feedback from auto grader

--- server/routes/index.js
+++ server/routes/index.js
@@ -132,14 +132,14 @@ router.get(`${carBaseUrl}/:carId`, isTokenValid, getACar);
 router.delete(`${carBaseUrl}/:carId`, isTokenValid, isAdmin, deleteCarAd);
 
 // Order routes
-// const orderBaseUrl = "/api/v1/order";
-// router.post(
-//   `${orderBaseUrl}`,
-//   isTokenValid,
-//   validateOrder,
-//   isCarExist,
-//   createOrder
-// );
+const orderBaseUrl = "/api/v1/order";
+router.post(
+  `${orderBaseUrl}`,
+  isTokenValid,
+  validateOrder,
+  isCarExist,
+  createOrder
+);
 // router.patch(
 //   `${orderBaseUrl}/:orderId/price`,
 //   isTokenValid,
